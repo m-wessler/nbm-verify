1ee4548825f3e8df50676dd1a24b457bcfbf8cec
m-wessler <m.wessler@utah.edu>
2020-10-30 00:05:08 -0600

    made progress on aggregating data over time but memory issues persist - need to come up with a better way to handle swap files or data in memory

--- scripts/extract_nbm_grid_variable.py
+++ scripts/extract_nbm_grid_variable.py
@@ -13,57 +13,84 @@ os.environ['OMP_NUM_THREADS'] = '1'
 
 nbm_dir = '/scratch/general/lustre/u1070830/nbm/'
 urma_dir = '/scratch/general/lustre/u1070830/urma/'
+tmp_dir = '/scratch/general/lustre/u1070830/tmp/'
+os.makedirs(tmp_dir, exist_ok=True)
+
 nbm_shape = (1051, 1132)
 
 def unpack_fhr(nbm_file, xthreshold, xinterval, returned=False):
     
     # WE NEED TO MATCH URMA IN HERE IF WE CAN! 
-    
-    print(nbm_file.split('/')[-2:])
-    
-    with pygrib.open(nbm_file) as grb:
         
-        msgs = grb.read()
-        if len(msgs) > 0:
-                    
-            _init = nbm_file.split('/')[-2:]
-            init = datetime.strptime(
-                _init[0] + _init[1].split('.')[1][1:-1], 
-                '%Y%m%d%H')
+    try:
+        with pygrib.open(nbm_file) as grb:
 
-            if init.hour % 6 != 0:
-                init -= timedelta(hours=1)
+            msgs = grb.read()
+            if len(msgs) > 0:
 
-            lats, lons = grb.message(1).latlons()
+                _init = nbm_file.split('/')[-2:]
+                init = datetime.strptime(
+                    _init[0] + _init[1].split('.')[1][1:-1], 
+                    '%Y%m%d%H')
 
-            valid = datetime.strptime(
-                str(msgs[0].validityDate) + '%02d'%msgs[0].validityTime, 
-                '%Y%m%d%H%M')
+                if init.hour % 6 != 0:
+                    init -= timedelta(hours=1)
+
+                lats, lons = grb.message(1).latlons()
+
+                valid = datetime.strptime(
+                    str(msgs[0].validityDate) + '%02d'%msgs[0].validityTime, 
+                    '%Y%m%d%H%M')
+
+                step = valid - init
+                lead = int(step.days*24 + step.seconds/3600)
+
+                tmpfile = tmp_dir + '%02dprobX%s_%s_f%03d'%(xinterval, str(xthreshold).replace('.', 'p'), init.strftime('%Y%m%d%H'), lead)
+
+                if not os.path.isfile(tmpfile + '.npy'):
+                    print(nbm_file.split('/')[-2:])
+
+                    for msg in msgs:
+
+                        if 'Probability of event above upper limit' in str(msg):
+
+                            interval = msg['stepRange'].split('-')
+                            interval = int(interval[1]) - int(interval[0])
+
+                            threshold = msg.upperLimit
+
+                            if ((threshold == xthreshold)&(interval == xinterval)):
+
+                                returned = True
+                                agg_data = np.array((init, valid, lead, msg.values), dtype=object)
+                                np.save(tmpfile, agg_data, allow_pickle=True)
+
+                    if not returned:
+
+                        agg_data = np.array((init, valid, lead, np.full(nbm_shape, fill_value=np.nan)), dtype=object)
+                        np.save(tmpfile, agg_data, allow_pickle=True)
+
+                else:
+                    print(nbm_file.split('/')[-2:], 'from file')
+                    agg_data = np.load(tmpfile + '.npy', allow_pickle=True)
+
+            else:
+                print('%s: No grib messages'%nbm_file.split('/')[-2:])
+    
+    except:
+        pass
+    
+    else:
+        
+        try:
+            grb.close()
+            del grb
+            gc.collect()
             
-            step = valid - init
-            lead = int(step.days*24 + step.seconds/3600)
-
-            for msg in msgs:
-
-                if 'Probability of event above upper limit' in str(msg):
-
-                    interval = msg['stepRange'].split('-')
-                    interval = int(interval[1]) - int(interval[0])
-
-                    threshold = msg.upperLimit
-
-                    if ((threshold == xthreshold)&(interval == xinterval)):
-                        
-                        returned = True
-                        return (init, valid, lead, msg.values)
-                    
-            if not returned:
-                return(init, valid, lead, np.full(nbm_shape, fill_value=np.nan))
-            
-        else:
-            print('%s: No grib messages'%nbm_file.split('/')[-2:])
-            
-    gc.collect()
+        except:
+            pass
+        
+        return agg_data
                 
 if __name__ == '__main__':
     
@@ -75,47 +102,55 @@ if __name__ == '__main__':
     
     # Build a list of inits
     inits = pd.date_range(
-        datetime(2020, 5, 18, 0), 
+        datetime(2020, 8, 15, 0), 
         datetime(2020, 10, 26, 23), 
         freq='6H')
-        
-    nbm_flist_agg = []
-    for init in inits:
-
-        try:
-            nbm_flist = sorted(glob(nbm_dir + init.strftime('%Y%m%d') + '/*t%02dz*'%init.hour))
-            nbm_flist[0]
-
-        except:
-            nbm_flist = sorted(glob(nbm_dir + init.strftime('%Y%m%d') + '/*t%02dz*'%(init+timedelta(hours=1)).hour))
-            
-        nbm_flist = [f for f in nbm_flist if 'idx' not in f]
-
-        if len(nbm_flist) > 0:
-            nbm_flist_agg.append(nbm_flist)
-            
-    nbm_flist_agg = np.hstack(nbm_flist_agg)
     
-    unpack_fhr_mp = partial(unpack_fhr, xinterval=extract_interval, xthreshold=extract_threshold)
+    outfile = './' + data_label + '.%s_%s.WR.nc'%(
+        inits[0].strftime('%Y%m%d%H'), 
+        inits[-1].strftime('%Y%m%d%H'))
     
-    # 128 workers ~ 100GB RAM
-    workers = 128
-    with mp.get_context('fork').Pool(workers) as p:
-        returns = p.map(unpack_fhr_mp, nbm_flist_agg, chunksize=1)
-        p.close()
-        p.join()
+    if not os.path.isfile(outfile):
         
-    returns = np.array([r for r in returns if r is not None], dtype=object)
-    init = returns[:, 0].astype(np.datetime64)
-    valid = returns[:, 1].astype(np.datetime64).reshape(len(np.unique(init)), -1)
-    lead = returns[:, 2].astype(np.int16).reshape(len(np.unique(init)), -1)
-    data = np.array([r for r in returns[:, 3]], dtype=np.int8).reshape(len(np.unique(init)), -1, nbm_shape[0], nbm_shape[1])
+        nbm_flist_agg = []
+        for init in inits:
 
-    valid = xr.DataArray(valid, name='valid', dims=('init', 'lead'), coords={'init':np.unique(init), 'lead':np.unique(lead)})
-    data = xr.DataArray(data, name='data_label', dims=('init', 'lead', 'y', 'x'), coords={'init':np.unique(init), 'lead':np.unique(lead)})
-    data = xr.merge([data, valid])
+            try:
+                nbm_flist = sorted(glob(nbm_dir + init.strftime('%Y%m%d') + '/*t%02dz*'%init.hour))
+                nbm_flist[0]
 
-    outfile = './' + data_label + '.%s_%s.WR.nc'%(inits[0].strftime('%Y%m%d%H'), inits[-1].strftime('%Y%m%d%H'))
-    data.to_netcdf(outfile)
+            except:
+                nbm_flist = sorted(glob(nbm_dir + init.strftime('%Y%m%d') + '/*t%02dz*'%(init+timedelta(hours=1)).hour))
+
+            nbm_flist = [f for f in nbm_flist if 'idx' not in f]
+
+            if len(nbm_flist) > 0:
+                nbm_flist_agg.append(nbm_flist)
+
+        nbm_flist_agg = np.hstack(nbm_flist_agg)
+
+        unpack_fhr_mp = partial(unpack_fhr, xinterval=extract_interval, xthreshold=extract_threshold)
+
+        # 128 workers ~ 1.2GB RAM/worker
+        workers = 86
+        with mp.get_context('fork').Pool(workers) as p:
+            returns = p.map(unpack_fhr_mp, nbm_flist_agg, chunksize=1)
+            p.close()
+            p.join()
+
+        returns = np.array([r for r in returns if r is not None], dtype=object)
+        init = returns[:, 0].astype(np.datetime64)
+        valid = returns[:, 1].astype(np.datetime64).reshape(len(np.unique(init)), -1)
+        lead = returns[:, 2].astype(np.int16).reshape(len(np.unique(init)), -1)
+        data = np.array([r for r in returns[:, 3]], dtype=np.int8).reshape(len(np.unique(init)), -1, nbm_shape[0], nbm_shape[1])
+
+        valid = xr.DataArray(valid, name='valid', dims=('init', 'lead'), coords={'init':np.unique(init), 'lead':np.unique(lead)})
+        data = xr.DataArray(data, name=data_label, dims=('init', 'lead', 'y', 'x'), coords={'init':np.unique(init), 'lead':np.unique(lead)})
+        data = xr.merge([data, valid])
+
+        data.to_netcdf(outfile)
+        
+    else:
+        data = xr.open_dataset(outfile)
     
     print('Done...')
